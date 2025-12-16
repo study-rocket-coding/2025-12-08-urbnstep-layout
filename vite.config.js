19b43109aa3d449b418c45b5cc95b5871dd47936
Chieh <secret12501250@gmail.com>
2025-12-16 17:02:00 +0800

    fix: update navigation links to point to the root path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/2025-12-08-urbnstep-layout/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        productDetail: resolve(__dirname, "product-detail.html"),
+      },
+    },
+  },
 });
